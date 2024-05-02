bc4aa29323727eb359983dd0ef9e9690f0491c6c
Jon <jonathan.woolfrey@gmail.com>
2024-05-02 18:27:52 +0100

    Saving work in progress

--- include/ActionServerBase.h
+++ include/ActionServerBase.h
@@ -2,13 +2,11 @@
 #include <rclcpp_action/rclcpp_action.hpp>                                                          // ROS2 action libaries
 #include <serial_link_interfaces/action/move_to_joint_positions.hpp>                                // Custom action built in another project
 
-
 // Short definitions for actions
 using MoveToJointPositions = serial_link_interfaces::action::MoveToJointPositions;
 
 // Short definitions for goal handler
 using JointPositionsManager = rclcpp_action::ServerGoalHandle<MoveToJointPositions>;
-
 
 template <class ControlType>
 class ActionServerBase : public rclcpp::Node
@@ -24,14 +22,16 @@ class ActionServerBase : public rclcpp::Node
           {
                using namespace std::placeholders;
                
-               // Create & advertise the action for moving to multiple joint positions
+              // Create & advertise the action for moving to multiple joint positions
               // this->_jointPositionsServer
               // = rclcpp_action::create_server<MoveToJointPositions>(this,
-               //                                                     "move_to_joint_positions",
-               //                                                     std::bind(&ActionServerBase::move_to_joint_positions))
+              //                                                     "move_to_joint_positions",
+              //                                                     std::bind(&ActionServerBase::move_to_joint_positions))
           }
           
      private:
+     
+          unsigned int _numberOfJoints;
      
           ControlType _controller;                                                                  // Template parameter for a RobotLibrary serial link controller
      
@@ -43,9 +43,9 @@ class ActionServerBase : public rclcpp::Node
            * @param request The Goal component of the MoveToJointPositions action
            * @return REJECT if request is not sound, otherwise ACCEPT_AND_EXECUTE
            */
-          static rclcpp_action::GoalResponse
+          rclcpp_action::GoalResponse
           move_to_joint_positions(const rclcpp_action::GoalUUID &uuid,
-                                  std::shared_ptr<const MoveToJointPositions::Goal> &request);
+                                  std::shared_ptr<const MoveToJointPositions::Goal> &request);        
           
           /**
            * Stop the robot from moving.
@@ -65,21 +65,50 @@ class ActionServerBase : public rclcpp::Node
 };                                                                                                  // Semicolon needed after class declaration
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
- //              Request that the robot move through multiple joint positions.                     //
+ //                     Move the robot through multiple joint configurations                       //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-//template <class ControlType>
-rclcpp_action::GoalResponse move_to_joint_positions(const rclcpp_action::GoalUUID &uuid,
+template <class ControlType>
+rclcpp_action::GoalResponse
+ActionServerBase<ControlType>::move_to_joint_positions(const rclcpp_action::GoalUUID &uuid,
                                                        std::shared_ptr<const MoveToJointPositions::Goal> &request)
 {
      (void)uuid;                                                                                    // This stops colcon from throwing a warning
-     (void)request;
- 
-     std::cout << "Worker bees can leave.\n";
-     std::cout << "Even drones can fly away.\n";
-     std::cout << "The Queen is their slave.\n";
+    
+     // Ensure the dimensions of inputs are sound
+     if(request->waypoints.size() < 2)
+     {
+          std::string errorMessage = "A minimum number of 2 waypoints is required to generate a "
+                                     "trajectory, but received only " + std::to_string(request->waypoints.size()) + ".";
+     }
+     else if(request->times.size() != request->waypoints.size())
+     {
+          std::string errorMessage = "Number of waypoints does not equal number of times ("
+                                   + std::to_string(request->waypoints.size()) + " =/= "
+                                   + std::to_string(request->times.size());
+                                   
+          RCLCPP_ERROR(this->get_logger(), errorMessage.c_str());                                   // Convert to char and publish
+          
+          return rclcpp_action::GoalResponse::REJECT;
+     }
+     else if(this->_controller->model->number_of_joints() != request->tracking_tolerance.size()
+          or this->_controller->model->number_of_joints() != request->final_tolerance.size())
+     {
+          std::string errorMessage = "This robot has "
+                                   + std::to_string(this->_controller->model->number_of_joints()) + 
+                                    " but the tracking tolerance array had " + request->tracking_tolerance.size() +
+                                    " elements, and the final tolerance array had "
+                                   + request->final_tolerance.size() + " elements.";
+          
+          RCLCPP_ERROR(this->get_logger(), errorMessage.c_str());
+          
+          return rclcpp_action::GoalResponse::REJECT;
+     }
+     
+     // 
      
      return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
-}
+} 
+
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
  //                   Solve the feedback control to track a joint trajectory                       //
