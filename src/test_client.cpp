f4c2c058aedbf8bfd0831290ec1615ef817e9a4e
Jon <jonathan.woolfrey@gmail.com>
2024-08-13 17:41:14 +0100

    Moved out of test folder

---
+++ src/test_client.cpp
@@ -0,0 +1,125 @@
+/**
+ * @file   test_client.cpp
+ * @author Jon Woolfrey
+ * @date   July 2024
+ * @brief  This is for testing action servers.
+ */
+
+#include <TrackJointTrajectory.h>
+#include <random>                                                                                   // For generating random numbers
+
+using JointControlAction = serial_link_interfaces::action::TrackJointTrajectory;
+
+
+int main(int argc, char **argv)
+{
+    if(argc != 3)
+    {
+        std::cerr << "[ERROR] [TEST CLIENT] Incorrect number of arguments. Usage:\n"
+                  << "ros2 run serial_link_action_server test_client robot_name numberOfJoints\n";
+        
+        return -1;
+    }
+    
+    std::string actionName = "track_joint_trajectory";                                              // Name of action
+    
+    int numJoints = std::stoi(argv[2]);                                                             // Convert to int
+    
+    rclcpp::init(argc, argv);                                                                       // Launches ROS2
+    
+    //////////////////////////////////////// Create client /////////////////////////////////////////
+    std::string name = std::string(argv[1]) + "_test_client";
+    
+    auto node = rclcpp::Node::make_shared(name);                                                    // Creates a node
+    
+    auto clientServer = rclcpp_action::create_client<JointControlAction>(node, actionName);         // Attach the node to this client
+    
+    RCLCPP_INFO(node->get_logger(),
+                std::string("Waiting for action '" + actionName + "' to be advertised...").c_str());
+   
+    ////////////////////////////////////// Wait for server /////////////////////////////////////////
+    if(not clientServer->wait_for_action_server(std::chrono::seconds(5)))
+    {
+        RCLCPP_ERROR(node->get_logger(),
+                     std::string("Action server '" + actionName + "' did not appear after waiting for 5 seconds. "
+                                 "Shutting down.").c_str());
+        
+        return -1;
+    }
+    else
+    {
+        RCLCPP_INFO(node->get_logger(), "Action server found.");
+    }
+    
+    //////////////////////////////////// Create trajectory points //////////////////////////////////
+    auto goal = JointControlAction::Goal();
+    
+    std::uniform_real_distribution<double> uniformDistribution(-1.0, 1.0);
+    std::default_random_engine randomEngine;
+    
+    serial_link_interfaces::msg::JointTrajectoryPoint startPoint, endPoint;
+    
+    startPoint.time =  0.0;
+    endPoint.time   = 10.0;
+    
+    for(int i = 0; i < numJoints; i++)
+    {
+         startPoint.position.push_back(uniformDistribution(randomEngine));
+         startPoint.velocity.push_back(0.0);
+         startPoint.acceleration.push_back(0.0);
+         
+         endPoint.position.push_back(uniformDistribution(randomEngine));
+         endPoint.velocity.push_back(0.0);
+         endPoint.acceleration.push_back(0.0);
+    }
+    
+    goal.points = {startPoint, endPoint};                                                           // Add them to the goal
+    goal.delay = 0.0;
+   
+    /////////////////////////////////////// Send to server /////////////////////////////////////////
+    RCLCPP_INFO(node->get_logger(), "Sending goal.");
+    auto sendGoalFuture = clientServer->async_send_goal(goal);
+    if(rclcpp::spin_until_future_complete(node, sendGoalFuture)!= rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_ERROR(node->get_logger(), "Failed to send goal.");
+        return -1;
+    }
+    
+    auto goalHandle = sendGoalFuture.get();
+    if(not goalHandle)
+    {
+        RCLCPP_ERROR(node->get_logger(), "Goal was rejected.");
+        return -1;
+    }
+    
+    ///////////////////////////////////// Wait for result //////////////////////////////////////////
+    auto resultFuture = clientServer->async_get_result(goalHandle);
+    if(rclcpp::spin_until_future_complete(node, resultFuture) != rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_ERROR(node->get_logger(), "Failed to get result.");
+        return -1;
+    }
+    
+    auto result = resultFuture.get();
+    if(result.code == rclcpp_action::ResultCode::SUCCEEDED)
+    {
+        RCLCPP_INFO(node->get_logger(), "Action succeeded.");
+        
+        for(auto &error : result.result->position_error)
+        {
+            std::cout << "---\n"
+                      << "Mean: " << error.mean << "\n"
+                      << "Variance: " << error.variance << "\n"
+                      << "Min: " << error.min << "\n"
+                      << "Max: " << error.max << "\n";
+        }
+    }
+    else
+    {
+        RCLCPP_ERROR(node->get_logger(), "Action failed.");
+    }
+
+    rclcpp::shutdown();                                                                             // Shuts down ROS2   
+    
+    return 0;                                                                                       // Shut down program; no errors.
+}
---
+++ src/velocity_control_server.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file   test_server.cpp
+ * @author Jon Woolfrey
+ * @date   June 2024
+ * @brief  This is for testing action servers.
+ */
+
+#include <TrackJointTrajectory.h>                                                                   // ROS2 action
+#include <RobotLibrary/SerialKinematicControl.h>                                                    // For serial link robots
+#include <ModelUpdater.h>                                                                           // Joint state subscriber
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);                                                                       // Launches ROS2
+    
+    auto node = std::make_shared<rclcpp::Node>("serial_link_control_parameters");                   // Create a node to access parameters
+
+    // Load simulation parameters from the server
+    std::string urdfLocation = node->declare_parameter<std::string>("urdf_location", "");
+    std::string endpointName = node->declare_parameter<std::string>("endpoint_name", "unnamed");
+    
+    try
+    {
+        KinematicTree robotModel(urdfLocation);                                                     // Create model
+        
+        SerialKinematicControl controller(&robotModel, endpointName);                               // Create controller, attach model
+        
+        // Create the nodes necessary for coordinating the control server
+        auto actionServer = std::make_shared<TrackJointTrajectory>(&controller);                    // Runs joint control mode
+        auto modelUpdater = std::make_shared<ModelUpdater>(&robotModel);                            // Reads & updates joint state
+        
+        // Create multi-thread executor and add nodes
+        rclcpp::executors::MultiThreadedExecutor executor;
+        executor.add_node(actionServer);
+        executor.add_node(modelUpdater);
+
+        executor.spin();                                                                            // Runs all the nodes on separate threads
+
+        rclcpp::shutdown();                                                                         // Shuts down ROS2   
+        
+        return 0;                                                                                   // Shut down program; no errors.        
+    }
+    catch(const std::exception &exception)
+    {
+        std::cerr << exception.what() << "\n";
+        
+        return -1;
+    }
+}
