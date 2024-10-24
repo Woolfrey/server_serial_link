bd901899bf46bd3263d3b5082de6447db8e233d9
Jon <jonathan.woolfrey@gmail.com>
2024-10-24 16:37:07 +0100

    Removed unecessary content for now

--- include/JointTrajectoryClient.h
+++ include/JointTrajectoryClient.h
@@ -15,41 +15,7 @@ class JointTrajectoryClient : public ActionClientBase<serial_link_action_server:
     
         JointTrajectoryClient(std::shared_ptr<rclcpp::Node> clientNode,
                               const std::string &actionName)
-                              : ActionClientBase(clientNode, actionName)
-        {
-            // Worker bees can leave.
-            // Even drones can fly away.
-            // The Queen is their slave.
-        }  
-    
-    private:
-        
-        /**
-         * Processes the response to an action request.
-         * @param future A future containing the result of the goal send operation.
-         */
-        void process_response(const typename rclcpp_action::ClientGoalHandle<Action>::SharedPtr &goalHandle)
-        {
-            if (!goalHandle)
-            {
-                RCLCPP_ERROR(_node->get_logger(), "Goal was rejected.");
-                return;
-            }
-
-            RCLCPP_INFO(_node->get_logger(), "Response received!");
-        }
-
-
-        /**
-         * Processes the result of an action.
-         * @param result The wrapped result of the action.
-         */
-        void process_result(const typename rclcpp_action::ClientGoalHandle<Action>::WrappedResult &result)
-        {
-            (void) result;
-            
-            RCLCPP_INFO(_node->get_logger(), "Action complete.");
-        }
+                              : ActionClientBase(clientNode, actionName) {}
 };
 
 #endif // JOINT_TRAJECTORY_CLIENT_H
