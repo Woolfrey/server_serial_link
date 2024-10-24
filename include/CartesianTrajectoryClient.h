995fbea0324cff80b28b6e8764886adeb72384c3
Jon <jonathan.woolfrey@gmail.com>
2024-10-24 16:21:26 +0100

    New derived class from ActionClientBase

---
+++ include/CartesianTrajectoryClient.h
@@ -0,0 +1,20 @@
+#ifndef CARTESIAN_TRAJECTORY_CLIENT_H
+#define CARTESIAN_TRAJECTORY_CLIENT_H
+
+#include "ActionClientBase.h"
+#include "serial_link_action_server/action/track_cartesian_trajectory.hpp"
+#include <rclcpp/rclcpp.hpp>
+
+class CartesianTrajectoryClient : public ActionClientBase<serial_link_action_server::action::TrackCartesianTrajectory>
+{
+    public:
+    
+        using Action = serial_link_action_server::action::TrackCartesianTrajectory;
+    
+        CartesianTrajectoryClient(std::shared_ptr<rclcpp::Node> clientNode,
+                                  const std::string &actionName)
+                                  : ActionClientBase(clientNode, actionName) {}
+};
+
+#endif // CARTESIAN_TRAJECTORY_CLIENT_H
+
