da176fc961155209db0b65304d880e76e654fcc7
Jon <jonathan.woolfrey@gmail.com>
2024-11-21 17:37:28 +0000

    Saving work in progress

--- src/velocity_control_server.cpp
+++ src/velocity_control_server.cpp
@@ -7,29 +7,29 @@
 
 #include <RobotLibrary/SerialKinematicControl.h>                                                    // For serial link robots
 #include <ModelUpdater.h>                                                                           // Joint state subscriber
-#include <TrackCartesianTrajectory.h>
-#include <TrackJointTrajectory.h>
+#include <server/TrackCartesianTrajectory.h>
+#include <server/TrackJointTrajectory.h>
 
 int main(int argc, char **argv)
 {
-    rclcpp::init(argc, argv);  // Launches ROS2
+    rclcpp::init(argc, argv);                                                                       // Launches ROS2
 
     try
     {
         auto paramNode = std::make_shared<rclcpp::Node>("param_loader");                            // Temporary node to load parameters
 
         // Load all parameters at once
-        std::string urdfLocation = paramNode->declare_parameter<std::string>("urdf_location", "");
+        std::string urdfLocation = paramNode->declare_parameter<std::string>("urdf", "");
         double kp = paramNode->declare_parameter<double>("kp", 10.0);
         double frequency = paramNode->declare_parameter<double>("frequency", 500.0);
-        std::string endpointName = paramNode->declare_parameter<std::string>("endpoint_name", "unnamed");
-        std::string controlTopicName = paramNode->declare_parameter<std::string>("control_topic_name", "joint_commands");
+        std::string endpointName = paramNode->declare_parameter<std::string>("endpoint", "unnamed");
+        std::string controlTopicName = paramNode->declare_parameter<std::string>("control_topic", "joint_commands");
 
         paramNode.reset();                                                                          // Free the node and its resources
 
         // Create model & controller
-        KinematicTree robotModel(urdfLocation);                                                     // Create the robot model
-        SerialKinematicControl controller(&robotModel, endpointName, frequency);                    // Create controller, attach model
+        RobotLibrary::KinematicTree robotModel(urdfLocation);                                       // Create the robot model
+        RobotLibrary::SerialKinematicControl controller(&robotModel, endpointName, frequency);      // Create controller, attach model
         controller.set_joint_gains(kp, 1.0);                                                        // Set control gains
         
         // Create nodes
@@ -37,15 +37,22 @@ int main(int argc, char **argv)
         auto serverNode = std::make_shared<rclcpp::Node>(robotModel.name() + "_action_server");     // Create server node
 
         // List actions, attach server node
-        TrackJointTrajectory jointTrajectoryServer(serverNode,
-                                                   &controller,
-                                                   "track_joint_trajectory",
-                                                   "joint_command_relay");
+        
+        std::mutex mutex;                                                                           // Stops 2 actions using robot simultaneously
+               
+        TrackJointTrajectory jointTrajectoryServer(
+            serverNode,
+            &controller,
+            &mutex,
+            "track_joint_trajectory",
+            "joint_command_relay");
                                                    
-        TrackCartesianTrajectory cartesianTrajectoryServer(serverNode,
-                                                           &controller,
-                                                           "track_cartesian_trajectory",
-                                                           "joint_command_relay");
+        TrackCartesianTrajectory cartesianTrajectoryServer(
+            serverNode,
+            &controller,
+            &mutex,
+            "track_cartesian_trajectory",
+            "joint_command_relay");
 
         // Add nodes to executor & spin
         rclcpp::executors::MultiThreadedExecutor executor;
