6805aefb7646d08746cc74af4470c86efaf7f437
Jon <jonathan.woolfrey@gmail.com>
2024-12-04 15:11:17 +0000

    Now inherits Interface class

--- include/client/ActionClientBase.h
+++ include/client/ActionClientBase.h
@@ -1,17 +1,24 @@
+/**
+ * @file   ActionClientBase.h
+ * @author Jon Woolfrey
+ * @data   October 2024
+ * @brief  This is an interface class for action clients in ROS2.
+ */
+
 #ifndef ACTION_CLIENT_BASE_H
 #define ACTION_CLIENT_BASE_H
 
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp_action/rclcpp_action.hpp>
+#include <client/ActionClientInterface.h>
 #include <memory>
 #include <string>
+#include <action_msgs/srv/cancel_goal.hpp>
 
 template <class Action>
-class ActionClientBase
+class ActionClientBase : public ActionClientInterface
 {
     public:
         
-        using ActionManager = rclcpp_action::ClientGoalHandle<Action>;                              ///< For easier referencing
+        using GoalHandle = rclcpp_action::ClientGoalHandle<Action>;                                 ///< For easier referencing
 
         /**
          * Constructor.
@@ -19,137 +26,211 @@ class ActionClientBase
          * @param actionName Must match what is advertised by the server.
          */
         ActionClientBase(std::shared_ptr<rclcpp::Node> clientNode,
-                         const std::string &actionName)
-                         : _node(clientNode),
-                           _actionClient(rclcpp_action::create_client<Action>(_node, actionName)) {}
+                         const std::string &actionName);
 
         /**
          * Send a request to execute an action.
          * @param goal The goal field of the action to be sent to the server.
-         */
-        void send_request(const typename Action::Goal::SharedPtr &goal)
-        {
-            using namespace std::placeholders;
-
-            if (!_actionClient->wait_for_action_server(std::chrono::seconds(1)))
-            {
-                RCLCPP_ERROR(_node->get_logger(), "Action server not available.");
-                return;
-            }
-
-            typename rclcpp_action::Client<Action>::SendGoalOptions options;                        // These are used to set callback functions
-
-            // Set up the callback for processing the goal response
-            options.goal_response_callback = std::bind(
-                &ActionClientBase::process_response, 
-                this, 
-                std::placeholders::_1
-            );
-
-            // Set up the callback for processing the result
-            options.result_callback = std::bind(
-                &ActionClientBase::process_result, 
-                this, 
-                std::placeholders::_1
-            );
-
-            auto goalHandleFuture = _actionClient->async_send_goal(*goal, options);                 // Send the goal
-        }
-
-        /**
-         *  Does this really need an explanation?
-         * @return True or false.
+         * @return A std::shared_future to the goal handle.
          */
         bool
-        action_is_running() const { return _actionManager != nullptr; }                             // Returns true if there is an active goal
- 
+        send_request(const typename Action::Goal::SharedPtr &goal);
+
         /**
          * It does what you'd expect.
          * @return False if no action is running.
          */
-        bool cancel_action()
+        bool
+        cancel_action() override;
+        
+        /**
+         * This overrides the method defined in the base class.
+         */
+        int8_t
+        status() const override
         {
-            if (_actionManager)
+            if(_goalHandle) return _goalHandle->get_status();
+            else            return 0;
+        }
+        
+        bool
+        is_running() const override
+        {
+            if(status() == 1
+            or status() == 2
+            or status() == 3)
             {
-                auto cancel_future = _actionClient->async_cancel_goal(_actionManager);
-                
-                if (cancel_future.wait_for(std::chrono::milliseconds(500)) == std::future_status::ready)
-                {
-                    _actionManager.reset();
-                }
-                else
-                {
-                    RCLCPP_WARN(_node->get_logger(), "Cancellation request timed out.");
-                }
-
                 return true;
             }
             else
             {
-                RCLCPP_WARN(_node->get_logger(), "No action is currently running.");
                 return false;
             }
         }
  
     protected:
         
-        std::shared_ptr<rclcpp::Node> _node;                                                        // Pointer to client node.
+        std::shared_ptr<rclcpp::Node> _node;                                                        ///< Pointer to client node.
+        
+        typename rclcpp_action::Client<Action>::SendGoalOptions _options;                           ///< These are used to set callback functions        
        
-        typename rclcpp_action::Client<Action>::SharedPtr _actionClient;                            // This is the foundation of the class
+        typename rclcpp_action::Client<Action>::SharedPtr _actionClient;                            ///< This is the foundation of the class
 
-        typename rclcpp_action::ClientGoalHandle<Action>::SharedPtr _actionManager;                 // Current goal handle
+        typename rclcpp_action::ClientGoalHandle<Action>::SharedPtr _goalHandle;                    ///< Current goal handle
+          
+        /**
+         * This method executes after requesting an action from a server, and its given an answer.
+         * @param GoalHandle A pointer to the goal handle associated with the action.
+         */
+        void
+        response_callback(const typename rclcpp_action::ClientGoalHandle<Action>::SharedPtr goalHandle);
         
         /**
-         * Processes the response to an action request.
-         * @param future A future containing the result of the goal send operation.
+         * This method executes when an action is finished.
+         * @param result The result portion of the associated goal field.
          */
-        void process_response(const typename rclcpp_action::ClientGoalHandle<Action>::SharedPtr &actionManager)
-        {
-            if (actionManager)
-            {
-                _actionManager = actionManager;                                                     // Store the goal handle
-            }
-            else
-            {
-                RCLCPP_INFO(_node->get_logger(), "Action request rejected.");
-            }
-        }
-
-        /**
-         * Processes the result of an action.
-         * @param result The wrapped result of the action.
-         */
-        void process_result(const typename rclcpp_action::ClientGoalHandle<Action>::WrappedResult &result)
-        {
-            switch (result.code)
-            {
-                case rclcpp_action::ResultCode::SUCCEEDED:
-                {
-                    RCLCPP_INFO(_node->get_logger(), "Action succeeded.");
-                    break;
-                }
-                case rclcpp_action::ResultCode::CANCELED:
-                {
-                    RCLCPP_INFO(_node->get_logger(), "Action was canceled.");
-                    break;
-                }
-                case rclcpp_action::ResultCode::ABORTED:
-                {
-                    RCLCPP_INFO(_node->get_logger(), "Action failed.");
-                    break;
-                }
-                default:
-                {
-                    RCLCPP_WARN(_node->get_logger(), "Unknown action result code.");
-                    break;
-                }
-            }
-
-            RCLCPP_INFO(_node->get_logger(), "Resetting goal handle.");
+        void
+        result_callback(const typename rclcpp_action::ClientGoalHandle<Action>::WrappedResult &result);
             
-            _actionManager.reset();                                                                 // Ensure the goal handle is reset after processing the result
-        }
+        /**
+         *
+         */
+        void
+        cancel_callback(const typename rclcpp_action::Client<Action>::CancelResponse::SharedPtr response);
 };
 
-#endif // ACTION_CLIENT_BASE_H
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                                           Constructor                                          //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Action>
+ActionClientBase<Action>::ActionClientBase(std::shared_ptr<rclcpp::Node> clientNode,
+                                                   const std::string &actionName)
+                                                   : ActionClientInterface(),
+                                                     _node(clientNode),
+                                                     _actionClient(rclcpp_action::create_client<Action>(_node, actionName))
+{
+    // Attach the response callback after an action request is sent
+    _options.goal_response_callback = std::bind
+    (
+        &ActionClientBase::response_callback,                                                       // Name of the method
+        this,                                                                                       // Attach this node
+        std::placeholders::_1                                                                       // I don't know what this does
+    );
 
+    // Attach the result callback for when an action is finished
+    _options.result_callback = std::bind
+    (
+        &ActionClientBase::result_callback,                                                         // Name of the method
+        this,                                                                                       // Attach this node
+        std::placeholders::_1                                                                       // I don't know what this does
+    );
+}
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                              Send an action request to the server                              //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Action>
+bool
+ActionClientBase<Action>::send_request(const typename Action::Goal::SharedPtr &goal)
+{
+    auto goalHandleFuture = _actionClient->async_send_goal(*goal, _options);                        // Send request, and link callback methods
+    
+    if (goalHandleFuture.wait_for(std::chrono::milliseconds(500)) == std::future_status::ready)
+    {
+        return goalHandleFuture.get() ? true : false;                                               // We want a fast return
+    }
+    else
+    {
+        RCLCPP_INFO(_node->get_logger(), "Failed to receive response from server after 500 ms.");
+        
+        return false;
+    }
+}
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                         Processes the response to an action request                            //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Action>
+void
+ActionClientBase<Action>::response_callback(const typename rclcpp_action::ClientGoalHandle<Action>::SharedPtr goalHandle)
+{
+    if(goalHandle)                                                                                  // Not a null pointer
+    {
+        _goalHandle = goalHandle;                                                                   // Save it internally
+    }
+    else
+    {
+        RCLCPP_INFO(_node->get_logger(), "Action request rejected by the server.");
+    }
+}
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                                  Executes when an action is finished.                          //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Action>
+void
+ActionClientBase<Action>::result_callback(const typename rclcpp_action::ClientGoalHandle<Action>::WrappedResult &result)
+{
+    switch (result.code)
+    {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+        {
+            RCLCPP_INFO(_node->get_logger(), "Action succeeded.");
+            break;
+        }
+        case rclcpp_action::ResultCode::CANCELED:
+        {
+            RCLCPP_INFO(_node->get_logger(), "Action was canceled.");
+            break;
+        }
+        case rclcpp_action::ResultCode::ABORTED:
+        {
+            RCLCPP_INFO(_node->get_logger(), "Action failed.");
+            break;
+        }
+        default:
+        {
+            RCLCPP_WARN(_node->get_logger(), "Unknown action result code.");
+            break;
+        }
+    }
+} 
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                            Cancel the action that is in progress                               //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Action>
+bool
+ActionClientBase<Action>::cancel_action()
+{
+    auto cancelFuture = _actionClient->async_cancel_goal(
+        _goalHandle,
+        [this](const typename rclcpp_action::Client<Action>::CancelResponse::SharedPtr response) {
+            this->cancel_callback(response);
+        }
+    );
+
+    if (cancelFuture.wait_for(std::chrono::milliseconds(500)) == std::future_status::ready)
+    {
+        return true;
+    }
+    else
+    {
+        RCLCPP_WARN(_node->get_logger(), "Request for action cancellation timed out.");
+        return false;
+    }
+}
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                                    Executes after cancelling                                   //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Action>
+void
+ActionClientBase<Action>::cancel_callback(const typename rclcpp_action::Client<Action>::CancelResponse::SharedPtr cancelResponse)
+{
+    (void)cancelResponse;
+    
+    // NEED TO FILL THIS IN
+}
+
+#endif
