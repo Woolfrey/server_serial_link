e7e67a560a2d98b6aa294cf79574dce91f6868ff
Jon <jonathan.woolfrey@gmail.com>
2024-12-18 09:59:59 +0000

    Improved documentation

--- src/mujoco_relay.cpp
+++ src/mujoco_relay.cpp
@@ -1,3 +1,10 @@
+/**
+ * @file   mujoco_relay.cpp
+ * @author Jon Woolfrey
+ * @data   November 2024
+ * @brief  A ROS2 node that converts joint commands to be used by the mujoco_ros2_interface package.
+ */
+
 #include "rclcpp/rclcpp.hpp"
 #include "serial_link_action_server/msg/joint_command.hpp"                                          // Custom message
 #include "std_msgs/msg/float64_multi_array.hpp"                                                     // For joint_commands topic
@@ -10,6 +17,12 @@ class MujocoRelayNode : public rclcpp::Node
         using JointCommandMsg = serial_link_action_server::msg::JointCommand;
         using Float64MultiArray = std_msgs::msg::Float64MultiArray;
         
+        /**
+         * Constructor.
+         * @nodeName As it says.
+         * @subscriptionName Name of the joint command topic.
+         * @publicationName Name for new topic to publish to.
+         */
         MujocoRelayNode(const std::string &nodeName = "mujoco_relay",
                         const std::string &subscriptionName = "joint_command_relay",
                         const std::string &publicationName = "joint_commands")
@@ -22,15 +35,19 @@ class MujocoRelayNode : public rclcpp::Node
                 std::bind(&MujocoRelayNode::relay, this, std::placeholders::_1)                     // Callback function for when message is received
             );
 
-            _jointCommandPublisher = this->create_publisher<Float64MultiArray>(publicationName, 1); // 
+            _jointCommandPublisher = this->create_publisher<Float64MultiArray>(publicationName, 1); 
         }
 
     private:
 
-        rclcpp::Subscription<JointCommandMsg>::SharedPtr _jointCommandSubscriber;
+        rclcpp::Subscription<JointCommandMsg>::SharedPtr _jointCommandSubscriber;                   ///< Subscriber for custom JointCommandMsg
         
-        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr _jointCommandPublisher;
+        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr _jointCommandPublisher;      ///< Publisher for array of joint commands
         
+        /*
+         * This method extracts the joint commands from the custom message and publishes it as an array.
+         * @param msg The JointCommandMsg message type.
+         */
         void relay(const JointCommandMsg::SharedPtr msg)
         {
             Float64MultiArray relayMsg;                                                             // Create Float64MultiArray on the stack
@@ -43,11 +60,14 @@ class MujocoRelayNode : public rclcpp::Node
         }
 };
 
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                                           MAIN                                                 //
+////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MujocoRelayNode>());
-    rclcpp::shutdown();
+    rclcpp::init(argc, argv);                                                                       // Start up ROS2
+    rclcpp::spin(std::make_shared<MujocoRelayNode>());                                              // Run the node
+    rclcpp::shutdown();                                                                             // As it says
     return 0;
 }
 
