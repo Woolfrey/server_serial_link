0f916eebfdf593f4d4e7baf3562bb0c232d69d92
Jon <jonathan.woolfrey@gmail.com>
2024-12-29 17:59:55 +1100

    Now loads joint & Cartesian trajectories from config file

--- src/demo_client.cpp
+++ src/demo_client.cpp
@@ -11,135 +11,12 @@
 #include "rclcpp/rclcpp.hpp"                                                                        // ROS2 C++ library
 #include "rclcpp_action/rclcpp_action.hpp"                                                          // ROS2 C++ action library
 #include <thread>                                                                                   // Threading (duh!)
+#include "Utilities.h"                                                                              // Useful functions
 
+using JointTrajectoryPoint     = serial_link_action_server::msg::JointTrajectoryPoint;
+using CartesianTrajectoryPoint = serial_link_action_server::msg::CartesianTrajectoryPoint;
 using TrackCartesianTrajectory = serial_link_action_server::action::TrackCartesianTrajectory;
 using TrackJointTrajectory     = serial_link_action_server::action::TrackJointTrajectory;
-using JointTrajectoryPoint     = serial_link_action_server::msg::JointTrajectoryPoint;
-
-/**
- * Store pre-defined joint trajectories and save them in a std::map.
- * @return A std::map where the key is the trajectory name specified in the YAML file.
- */
-std::map<std::string, std::vector<JointTrajectoryPoint>>
-load_joint_configurations()
-{
-    std::map<std::string,std::vector<JointTrajectoryPoint>> jointConfigurations;                    // We want to return this
-    
-    auto configNode = rclcpp::Node::make_shared("joint_configurations");                            // Create node to host parameters
-    
-    // Acquire number of joints
-    configNode->declare_parameter("number_of_joints", 0);
-    int numJoints;
-    configNode->get_parameter("number_of_joints", numJoints);
-    
-    // Get a list of configuration names
-    configNode->declare_parameter("names", std::vector<std::string>{});                             // Declare name list  
-    std::vector<std::string> names = {};                                                            // Storage location for names  
-    configNode->get_parameter("names", names);                                                      // Get the values from the server
-    
-    // Cycle through all the named configurations, extract waypoints
-    for(auto name : names)
-    {
-        // Declare the position, time parameters
-        configNode->declare_parameter(name+".positions", std::vector<double>{});
-        configNode->declare_parameter(name+".times", std::vector<double>{});
-        
-        // Create storage location
-        std::vector<double> positions;
-        std::vector<double> times;
-        
-        // Obtain from parameter server
-        configNode->get_parameter(name+".positions", positions);
-        configNode->get_parameter(name+".times", times);
-        
-        // Ensure dimensions are correct
-        if (positions.size() % numJoints != 0)
-        {
-            throw std::invalid_argument
-            (
-                "Size of position array (" + std::to_string(positions.size()) + 
-                ") not divisible by number of joints (" + std::to_string(numJoints) + ")."
-            );
-        }
-     
-        // Ensure number of waypoints matches number of times
-        if(positions.size()/numJoints != times.size())
-        {
-            throw std::invalid_argument
-            (
-                "Number of waypoints (" + std::to_string(positions.size()/numJoints) + ") "
-                "does not match number of times (" + std::to_string(times.size()) + ")."
-            );
-        }
-        
-        // Put the positions & times together to define a trajectory
-        std::vector<JointTrajectoryPoint> points;
-        
-        for (size_t i = 0; i < times.size(); ++i)
-        {
-            JointTrajectoryPoint point;
-            
-            point.time = times[i];
-            
-            std::vector<double> joint_positions(positions.begin() + i * numJoints,
-                                                positions.begin() + (i + 1) * numJoints);
-            
-            point.position = joint_positions;                                                       // Assign the extracted positions
-            
-            points.push_back(point);                                                                // Add the point to the trajectory
-        }
-        
-        jointConfigurations.emplace(name, points);
-    }
-    
-    return jointConfigurations;
-}
-
-/**
- * This function manages the asynchronous cancellation sequence.
- * @param activeClient A pointer to the action client that is currently running.
- * @return True if/when successful, false if there was a problem.
- */
-bool
-stop_robot(ActionClientInterface *activeClient)
-{
-    if (activeClient == nullptr)
-    {
-        return true;                                                                                // No active action
-    }
-
-    // Possible return codes:
-    // 0 = Unknown
-    // 1 = Accepted
-    // 2 = Executing
-    // 3 = Canceling
-    // 4 = Succeeded
-    // 5 = Canceled
-    // 6 = Aborted
-
-    if (activeClient->status() == 0)
-    {
-        return false; // A problem
-    }
-
-    // If running, cancel
-    if (activeClient->status() == 1
-    or  activeClient->status() == 2)
-    {
-        activeClient->cancel_action();
-    }
-
-    // Wait until action is resolved
-    while (activeClient->status() != 4 &&
-           activeClient->status() != 5 &&
-           activeClient->status() != 6)
-    {      
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));                                 // Wait for 100ms
-    }
-
-    return true;
-}
-
   /////////////////////////////////////////////////////////////////////////////////////////////////////
  //                                          MAIN                                                   //
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -148,8 +25,8 @@ int main(int argc, char **argv)
     rclcpp::init(argc, argv);                                                                       // Starts up ROS2
    
     auto clientNode = rclcpp::Node::make_shared("serial_link_demo_client");                         // Create client node and advertise its name
-
-    std::map<std::string, std::vector<JointTrajectoryPoint>> jointConfigurations = load_joint_configurations();
+    auto jointConfigurations = load_joint_configurations();                                         // From the parameter server
+    auto endpointPoses = load_endpoint_poses();                                                     // From the parameter server
 
     // Create the action clients, attach to node
     JointTrajectoryClient jointTrajectoryClient(clientNode, "track_joint_trajectory", true);
@@ -167,10 +44,15 @@ int main(int argc, char **argv)
         
         if (commandPrompt == "options")
         {
-            RCLCPP_INFO(clientNode->get_logger(), "Available configurations:");
-            for (const auto& config : jointConfigurations)
+            RCLCPP_INFO(clientNode->get_logger(), "Available joint commands:");
+            for (const auto &config : jointConfigurations)
             {
                 RCLCPP_INFO(clientNode->get_logger(), "- %s", config.first.c_str());
+            }
+            RCLCPP_INFO(clientNode->get_logger(), "Available Cartesian commands:");
+            for(const auto &pose : endpointPoses)
+            {
+                RCLCPP_INFO(clientNode->get_logger(), "- %s", pose.first.c_str());
             }
         }
         else if (commandPrompt == "close")
@@ -191,27 +73,47 @@ int main(int argc, char **argv)
             
             if (iterator != jointConfigurations.end())
             {   
-                if (activeClient != nullptr and activeClient->is_running()) stop_robot(activeClient);
+                if (activeClient != nullptr and activeClient->is_running())
+                {
+                    stop_robot(activeClient);
+                }
                 
-                // Create goal and attach trajectory points
-                auto goal = std::make_shared<TrackJointTrajectory::Goal>();
+                auto goal = std::make_shared<TrackJointTrajectory::Goal>();                         // Generate goal object
                 
-                goal->points = iterator->second;
+                goal->points = iterator->second;                                                    // Attach the joint trajectory
                 
-                RCLCPP_INFO(clientNode->get_logger(), "Moving to `%s` configuration(s).", commandPrompt.c_str());
+                RCLCPP_INFO(clientNode->get_logger(), "Moving to `%s` configuration(s).", commandPrompt.c_str()); // Inform user
 
-                jointTrajectoryClient.send_request(goal);
+                jointTrajectoryClient.send_request(goal);                                           // Send request to client
                 
-                activeClient = &jointTrajectoryClient;               
+                activeClient = &jointTrajectoryClient;                                             
             }
             else
             {
-                RCLCPP_WARN(clientNode->get_logger(), "Configuration '%s' not found.", commandPrompt.c_str());
+                auto iterator = endpointPoses.find(commandPrompt);
+                
+                if (iterator != endpointPoses.end())
+                {   
+                    if (activeClient != nullptr and activeClient->is_running())
+                    {
+                        stop_robot(activeClient);
+                    }
+                    
+                    auto goal = std::make_shared<TrackCartesianTrajectory::Goal>();                 // Generate goal object
+                    
+                    goal->points = iterator->second;                                                // Attach the joint trajectory
+                    
+                    RCLCPP_INFO(clientNode->get_logger(), "Moving `%s` .", commandPrompt.c_str());  // Inform user
+
+                    cartesianTrajectoryClient.send_request(goal);                                   // Send request to client
+                    
+                    activeClient = &cartesianTrajectoryClient;                                             
+                }
+                else
+                {
+                    RCLCPP_WARN(clientNode->get_logger(), "Unknown command. Type 'options' to see a list.");
+                }
             }
-        }
-        else
-        {
-            RCLCPP_WARN(clientNode->get_logger(), "Unknown command. Type 'options' to see a list.");
         }
     }
 
