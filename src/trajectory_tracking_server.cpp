ea9d2df07c21457ebde1de647d9bde4e7638d9b0
Jon <jonathan.woolfrey@gmail.com>
2025-02-21 17:02:12 +0000

    Seems to be up and running?

---
+++ src/trajectory_tracking_server.cpp
@@ -0,0 +1,81 @@
+/**
+ * @file   velocity_control_server.cpp
+ * @author Jon Woolfrey
+ * @date   June 2024
+ * @brief  This is testing & demonstration.
+ */
+
+#include <ModelUpdater.h>                                                                           // Joint state subscriber
+#include <RobotLibrary/Control/SerialKinematicControl.h>                                            // For serial link robots
+#include <TrackCartesianTrajectory.h>
+#include <TrackJointTrajectory.h>
+#include <Utilities.h>
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                                            MAIN                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);                                                                       // Launches ROS2
+
+    try
+    {
+        auto paramNode = std::make_shared<rclcpp::Node>("param_loader");                            // Temporary node to load parameters
+
+        // Load all parameters at once
+        std::string urdfLocation = paramNode->declare_parameter<std::string>("urdf", "");
+        double frequency = paramNode->declare_parameter<double>("frequency", 500.0);
+        std::string endpointName = paramNode->declare_parameter<std::string>("endpoint", "unnamed");  
+        std::string controlTopicName = paramNode->declare_parameter<std::string>("control_topic", "joint_commands");
+       
+        paramNode.reset();                                                                          // Free the node and its resources
+
+        // Create model & controller
+        RobotLibrary::Model::KinematicTree robotModel(urdfLocation);                                // Create the robot model
+        RobotLibrary::Control::SerialKinematicControl controller(&robotModel, endpointName, frequency); // Create controller, attach model
+        
+        if(not set_control_parameters(controller))
+        {
+            throw std::runtime_error("Failed to set control parameters for some reason.");
+        }
+          
+        // Create nodes
+        auto modelUpdaterNode = std::make_shared<ModelUpdater>(&robotModel);                        // Reads & updates joint state
+        auto serverNode = std::make_shared<rclcpp::Node>(robotModel.name() + "_action_server");     // Create server node
+
+        // List actions, attach server node      
+        std::mutex mutex;                                                                           // Stops 2 actions using robot simultaneously
+               
+        TrackJointTrajectory jointTrajectoryServer(
+            serverNode,
+            &controller,
+            &mutex,
+            "track_joint_trajectory",
+            "joint_command_relay");
+                                                   
+        TrackCartesianTrajectory cartesianTrajectoryServer(
+            serverNode,
+            &controller,
+            &mutex,
+            "track_cartesian_trajectory",
+            "joint_command_relay");
+
+        // Add nodes to executor & spin
+        rclcpp::executors::MultiThreadedExecutor executor;
+        executor.add_node(modelUpdaterNode);                                                        // Add model updater node
+        executor.add_node(serverNode);                                                              // Add server node
+
+        executor.spin();                                                                            // Run the node(s)
+
+        rclcpp::shutdown();                                                                         // As it says
+        
+        return 0;                                                                                   // No errors
+    }
+    catch (const std::exception &exception)
+    {
+        std::cerr << exception.what() << "\n";
+        
+        return -1;                                                                                  // Error occurred
+    }
+}
+
