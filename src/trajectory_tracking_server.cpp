79f7484b3131fea3119f0bcf0d15c4fb14e9f742
Jon <jonathan.woolfrey@gmail.com>
2025-03-20 16:01:15 +0000

    I don't remember what I changed, but it's working

--- src/trajectory_tracking_server.cpp
+++ src/trajectory_tracking_server.cpp
@@ -18,65 +18,68 @@ int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);                                                                       // Launches ROS2
 
-    unsigned int returnCode = 0;
-
+    // Ensure sufficient number of arguments is provided
     if (argc < 5)
     {
-        RCLCPP_ERROR(rclcpp::get_logger("main"),
-                     "Invalid number of arguments. Usage: <executable> <urdf_path> <endpoint_name> <control_topic_name> <joint_state_topic_name>");
-        returnCode = 1;
-    }
-    else
-    {  
-        // For clarity:
-        std::string urdfPath        = argv[1];
-        std::string endpointName    = argv[2];
-        std::string controlTopic    = argv[3];
-        std::string jointStateTopic = argv[4];
-            
-        try 
-        {
-            auto model = std::make_shared<RobotLibrary::Model::KinematicTree>(urdfPath);            // Generate dynamic model
-            
-            auto modelUpdaterNode = std::make_shared<ModelUpdater>(model, jointStateTopic);         // Create node for updating joint state
-            
-            auto serverNode = std::make_shared<rclcpp::Node>(model->name()+"_action_server");       // Create action server nodes
-            
-            auto controller = std::make_shared<RobotLibrary::Control::SerialKinematicControl>(model, endpointName, load_control_parameters(serverNode));
-              
-            // Declare action servers
-            auto mutex = std::make_shared<std::mutex>(); 
-       
-            TrackJointTrajectory jointTrajectoryServer(
-                serverNode,
-                controller,
-                mutex,
-                "track_joint_trajectory",
-                controlTopic);
-                                                   
-            TrackCartesianTrajectory cartesianTrajectoryServer(
-                serverNode,
-                controller,
-                mutex,
-                "track_cartesian_trajectory",
-                controlTopic);
-                     
-            // Add nodes to executor and spin
-            rclcpp::executors::MultiThreadedExecutor executor;
-            executor.add_node(modelUpdaterNode);
-            executor.add_node(serverNode);
-            executor.spin();
+        RCLCPP_ERROR(rclcpp::get_logger("main"), "Invalid number of arguments. "
+                     "Usage: <executable> <urdf_path> <endpoint_name> <control_topic_name> <joint_state_topic_name>");
 
-        }
-        catch(const std::exception &exception)
-        {
-            RCLCPP_ERROR(rclcpp::get_logger("main"), exception.what());
-            
-            returnCode = 1;
-        }  
+        rclcpp::shutdown();
+        
+        return 1;
     }
-    
-    rclcpp::shutdown();
+ 
+    // For clarity:
+    std::string urdfPath        = argv[1];
+    std::string endpointName    = argv[2];
+    std::string controlTopic    = argv[3];
+    std::string jointStateTopic = argv[4];
+        
+    try 
+    {
+        auto model = std::make_shared<RobotLibrary::Model::KinematicTree>(urdfPath);                // Generate dynamic model
+        
+        auto modelUpdaterNode = std::make_shared<ModelUpdater>(model, jointStateTopic);             // Create node for updating joint state
+        
+        auto serverNode = std::make_shared<rclcpp::Node>(model->name()+"_action_server");           // Create action server nodes
+        
+        auto controller = std::make_shared<RobotLibrary::Control::SerialKinematicControl>(model, endpointName, load_control_parameters(serverNode));
+          
+        // Declare action servers
+        auto mutex = std::make_shared<std::mutex>();                                                // This stops 2 actions using the robot at the same time
 
-    return returnCode;
+        TrackJointTrajectory jointTrajectoryServer(
+            serverNode,
+            controller,
+            mutex,
+            "track_joint_trajectory",
+            controlTopic);
+                                               
+        TrackCartesianTrajectory cartesianTrajectoryServer(
+            serverNode,
+            controller,
+            mutex,
+            "track_cartesian_trajectory",
+            controlTopic);
+                 
+        // Add nodes to executor and spin
+        rclcpp::executors::MultiThreadedExecutor executor;
+        executor.add_node(modelUpdaterNode);
+        executor.add_node(serverNode);
+        
+        executor.spin();
+        
+        rclcpp::shutdown();
+        
+        return 0;
+
+    }
+    catch(const std::exception &exception)
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("main"), exception.what());
+        
+        rclcpp::shutdown();
+        
+        return 1;
+    }  
 }
